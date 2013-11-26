32757fc2deaf8bc5766426622be2177a8d230370
erikm13 <erikmikalsen94@gmail.com>
2013-11-26 11:19:33 +0100

    26/11/13
    Versjonskontroll 26/11/13

---
+++ js/slideshow.js
@@ -0,0 +1,14 @@
+$('.fadein img:gt(0)').hide();
+setInterval(function(){ },3000);
+$('.fadein :first-child').fadeOut()
+.next('img').fadeIn()
+.end().appendTo('.fadein')
+
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      3000);
+});
